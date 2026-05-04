8a9f3ece88330582ad414cc40f7948d39b2bb9e5
york <york8w878@gmail.com>
2026-05-05 01:28:19 +0800

    Initial Commit

---
+++ LunarGlass/LunarGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarGlassApp.swift
+//  LunarGlass
+//
+//  Created by York on 2026/5/5.
+//
+
+import SwiftUI
+
+@main
+struct LunarGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
